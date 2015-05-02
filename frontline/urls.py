193bbad12e56d210e553b60b77e275347fc29a8f
Blake Thompson <bthompson@mmgyglobal.com>
2015-05-02 16:30:24 -0500

    blake unchained

--- frontline/urls.py
+++ frontline/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', 'frontline.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^games/new$', 'game.views.new', name='new-game'),
 )
--- game/views.py
+++ game/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.forms import ModelForm
+from game.models import Game
+import datetime
 # Create your views here.
+def new(request):
+    class GameForm(ModelForm):
+        class Meta:
+            model = Game
+            fields = ['title']
+
+
+    form = GameForm()
+    context = { 'gameForm': form.as_p() }
+
+    if request.method == 'POST':
+
+        game = GameForm(request.POST)
+
+        if game.is_valid():
+            game.save()
+            context['game'] = game.data['title']
+
+    return render(request, 'frontline/new-game.html', context)
